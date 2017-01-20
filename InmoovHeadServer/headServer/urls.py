ed47d2768d81fbee1d480481e78b3920dcd31b81
Dorian <208108@supinfo.com>
2017-01-20 12:37:30 +0100

    Ajout des templates pour l'authentification

--- InmoovHeadServer/headServer/urls.py
+++ InmoovHeadServer/headServer/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^arduino/', views.apiArduino),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url('^', include('django.contrib.auth.urls')),
 ]
