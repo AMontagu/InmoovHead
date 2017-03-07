c16e1f0da8911b55af30c31baa0f2696b6108984
Dorian <208108@supinfo.com>
2017-03-07 16:38:37 +0100

    First intergrate movement

--- InmoovHeadServer/headServer/routing.py
+++ InmoovHeadServer/headServer/routing.py
@@ -3,6 +3,7 @@ from test2.consumers import ws_add, ws_message, ws_disconnect, TtsJsonConsumer
 
 apiSocket = [
 	route_class(TtsJsonConsumer, path=r"^/tts/"),
+	route_class(TtsJsonConsumer, path=r"^/movement/"),
 	route("websocket.connect", ws_add),  # default for getting connection not in hease protocol remove in release
 	route("websocket.receive", ws_message),  # default for getting message not in hease protocol remove in release
 	route("websocket.disconnect", ws_disconnect),
@@ -12,4 +13,3 @@ apiSocket = [
 channel_routing = [
 	include(apiSocket, path=r'^/ws')
 ]
-
